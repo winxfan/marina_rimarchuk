da8ad2f2f6f16ecf242d1be02b52c282b1d89dba
tvdmitry <dmitry.tulupov68@gmail.com>
2024-01-24 22:01:16 +0300

    fix: Add back path

--- src/pages/statistics/components/StatisticTasks.tsx
+++ src/pages/statistics/components/StatisticTasks.tsx
@@ -3,72 +3,75 @@ import React from 'react';
 import { AccordionComponent } from '../../../modules/accordion/AccordionComponent';
 import { HeaderPage } from '../../../modules/header/components/HeaderPage';
 import { ProgressBar } from '../../../modules/progressBar/ProgressBar';
+import { useBackButton } from '../../../utils/hooks/useBackButton';
 import { IAccordionContent } from '../../../utils/types/statistic';
 import css from './StatisticTasks.module.scss';
 
+const levelData: IAccordionContent[] = [
+    {
+        id: '1',
+        title: 'Звездочка',
+        content: [
+            {
+                title: 'Куплено 1/3 методичек',
+                progress: {
+                    component: <ProgressBar percent={'1/3'} />,
+                    id: '6',
+                },
+            },
+            {
+                title: '3/20 дней подряд заполняется трекер воды',
+                progress: {
+                    component: <ProgressBar percent={'3/20'} />,
+                    id: '7',
+                },
+            },
+        ],
+    },
+    {
+        id: '2',
+        title: 'Звезда',
+        content: [
+            {
+                title: 'Куплено 1/3 методичек',
+                progress: {
+                    component: <ProgressBar percent={'1/3'} />,
+                    id: '8',
+                },
+            },
+            {
+                title: '3/20 дней подряд заполняется трекер воды',
+                progress: {
+                    component: <ProgressBar percent={'3/20'} />,
+                    id: '9',
+                },
+            },
+        ],
+    },
+    {
+        id: '3',
+        title: 'Легенда',
+        content: [
+            {
+                title: 'Куплено 1/3 методичек',
+                progress: {
+                    component: <ProgressBar percent={'1/3'} />,
+                    id: '10',
+                },
+            },
+            {
+                title: '3/20 дней подряд заполняется трекер воды',
+                progress: {
+                    component: <ProgressBar percent={'3/20'} />,
+                    id: '11',
+                },
+            },
+        ],
+    },
+];
+
 export const StatisticTasks = () => {
-    const levelData: IAccordionContent[] = [
-        {
-            id: '1',
-            title: 'Звездочка',
-            content: [
-                {
-                    title: 'Куплено 1/3 методичек',
-                    progress: {
-                        component: <ProgressBar percent={'1/3'} />,
-                        id: '6',
-                    },
-                },
-                {
-                    title: '3/20 дней подряд заполняется трекер воды',
-                    progress: {
-                        component: <ProgressBar percent={'3/20'} />,
-                        id: '7',
-                    },
-                },
-            ],
-        },
-        {
-            id: '2',
-            title: 'Звезда',
-            content: [
-                {
-                    title: 'Куплено 1/3 методичек',
-                    progress: {
-                        component: <ProgressBar percent={'1/3'} />,
-                        id: '8',
-                    },
-                },
-                {
-                    title: '3/20 дней подряд заполняется трекер воды',
-                    progress: {
-                        component: <ProgressBar percent={'3/20'} />,
-                        id: '9',
-                    },
-                },
-            ],
-        },
-        {
-            id: '3',
-            title: 'Легенда',
-            content: [
-                {
-                    title: 'Куплено 1/3 методичек',
-                    progress: {
-                        component: <ProgressBar percent={'1/3'} />,
-                        id: '10',
-                    },
-                },
-                {
-                    title: '3/20 дней подряд заполняется трекер воды',
-                    progress: {
-                        component: <ProgressBar percent={'3/20'} />,
-                        id: '11',
-                    },
-                },
-            ],
-        },
-    ];
+    useBackButton('/');
 
     return (
         <div className={css.statisticTasks}>
