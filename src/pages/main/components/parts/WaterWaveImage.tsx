87cc982daa22ca71361f82d1c4210a7ea0a10799
tvdmitry <dmitry.tulupov68@gmail.com>
2024-02-06 17:10:46 +0300

    fix: refactoring code

--- src/pages/main/components/parts/WaterWaveImage.tsx
+++ src/pages/main/components/parts/WaterWaveImage.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import css from './WaterWaveImage.module.scss';
+import css from './WaterTracker.module.scss';
 
 export type WaterWaveImageProps = {
     adjustedHeight: number;
@@ -10,58 +10,59 @@ const WaterWaveImage: FC<WaterWaveImageProps> = (props) => {
     const { adjustedHeight } = props;
 
     return (
-        <></>
-        /*  <svg
-            width="100%"
-            height={adjustedHeight + 25}
-            viewBox={`0 0 100% ${adjustedHeight}`}
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className={css.waveImage}
-            style={{ width: '100%' }}
-        >
-            <path
-                d="M-1 11.0243C230.113 -17.4737 225 79.5 562 1.02426V399H-1V11.0243Z"
-                fill="url(#paint0_linear_1040_7211)"
-                stroke="url(#paint1_linear_1040_7211)"
-            >
-                <animateTransform
-                    attributeName="transform"
-                    attributeType="XML"
-                    type="translate"
-                    dur="2s"
-                    keyTimes="0; 0.5; 1"
-                    repeatCount="indefinite"
-                    values="0 0; 0 5; 0 0"
-                    calcMode="spline"
-                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-                />
-            </path>
-            <defs>
-                <linearGradient
-                    id="paint0_linear_1040_7211"
-                    x1="231"
-                    y1="5"
-                    x2="231"
-                    y2="349"
-                    gradientUnits="userSpaceOnUse"
+        <div className={css.waterTrackerProgress} style={{ transform: `translateY(${-adjustedHeight}px)` }}>
+            <div className={css.background}>
+                <svg
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 1600 990"
                 >
-                    <stop stopColor="#4460F7" />
-                    <stop offset="1" stopColor="#12237A" />
-                </linearGradient>
-                <linearGradient
-                    id="paint1_linear_1040_7211"
-                    x1="231"
-                    y1="5"
-                    x2="231"
-                    y2="349"
-                    gradientUnits="userSpaceOnUse"
-                >
-                    <stop stopColor="white" stopOpacity="0.36" />
-                    <stop offset="1" stopOpacity="0" />
-                </linearGradient>
-            </defs>
-        </svg>*/
+                    <defs>
+                        <linearGradient id="bg" x2="0%" y2="100%">
+                            <stop offset="0%" stopColor="rgb(68, 96, 246)"></stop>
+                            <stop offset="100%" stopColor="rgb(18, 35, 122)"></stop>
+                        </linearGradient>
+                        <path
+                            id="wave"
+                            fill="url(#bg)"
+                            d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+      s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
+                        />
+                    </defs>
+                    <g>
+                        <use xlinkHref="#wave" opacity=".3">
+                            <animateTransform
+                                attributeName="transform"
+                                attributeType="XML"
+                                type="translate"
+                                dur="8s"
+                                calcMode="spline"
+                                values="270 230; -334 180; 270 230"
+                                keyTimes="0; .5; 1"
+                                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                                repeatCount="indefinite"
+                            />
+                        </use>
+                        <use xlinkHref="#wave" opacity=".9">
+                            <animateTransform
+                                attributeName="transform"
+                                attributeType="XML"
+                                type="translate"
+                                dur="4s"
+                                calcMode="spline"
+                                values="0 230;-140 200;0 230"
+                                keyTimes="0; .4; 1"
+                                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                                repeatCount="indefinite"
+                            />
+                        </use>
+                    </g>
+                </svg>
+            </div>
+        </div>
     );
 };
 
