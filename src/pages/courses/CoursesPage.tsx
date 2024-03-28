cc9527a19a5d1edc2c651568d47b075ccd09b750
tvdmitry <dmitry.tulupov68@gmail.com>
2024-03-28 11:19:14 +0300

    fix: Fix url videos

--- src/pages/courses/CoursesPage.tsx
+++ src/pages/courses/CoursesPage.tsx
@@ -36,7 +36,7 @@ export const dataCourses: ICourses[] = [
                         title: 'О важности воды, которую недооценивают',
                         description: 'Урок 1',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%9E%20%D0%B2%D0%B0%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20%D0%B2%D0%BE%D0%B4%D0%B8%D1%87%D0%BA%D0%B8%20%F0%9F%92%A6%F0%9F%92%A6%F0%9F%92%A6.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%9E%20%D0%B2%D0%B0%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20%D0%B2%D0%BE%D0%B4%D0%B8%D1%87%D0%BA%D0%B8%20%F0%9F%92%A6%F0%9F%92%A6%F0%9F%92%A6.mp4',
                     },
                     {
                         id: '12',
@@ -44,7 +44,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Про очищение организма, кому это надо',
                         description: 'Урок 2',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%9F%D1%80%D0%BE%20%D0%BE%D1%87%D0%B8%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%BC%D0%B0%F0%9F%8C%BF%F0%9F%8C%BF%F0%9F%8C%BF.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%9F%D1%80%D0%BE%20%D0%BE%D1%87%D0%B8%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%BC%D0%B0%F0%9F%8C%BF%F0%9F%8C%BF%F0%9F%8C%BF.mp4',
                     },
                     {
                         id: '13',
@@ -52,7 +52,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Про углеводы. То, что вы должны знать прямо сейчас',
                         description: 'Урок 3',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%9F%D1%80%D0%BE%20%D1%83%D0%B3%D0%BB%D0%B5%D0%B2%D0%BE%D0%B4%D1%8B%20%F0%9F%8D%8E%F0%9F%91%80.mp4',
+                        url: 'https://content-water.plutus-fin.ru//videos/%D0%9F%D1%80%D0%BE%20%D1%83%D0%B3%D0%BB%D0%B5%D0%B2%D0%BE%D0%B4%D1%8B%20%F0%9F%8D%8E%F0%9F%91%80.mp4',
                     },
                     {
                         id: '14',
@@ -60,7 +60,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Жиры и молодость. Прямая связь',
                         description: 'Урок 4',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%9F%D0%A0%D0%9E%20%D0%92%D0%90%D0%96%D0%9D%D0%9E%D0%A1%D0%A2%D0%AC%20%D0%96%D0%98%D0%A0%D0%9E%D0%92%20%D0%92%20%D0%A0%D0%90%D0%A6%D0%98%D0%9E%D0%9D%D0%95.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%9F%D0%A0%D0%9E%20%D0%92%D0%90%D0%96%D0%9D%D0%9E%D0%A1%D0%A2%D0%AC%20%D0%96%D0%98%D0%A0%D0%9E%D0%92%20%D0%92%20%D0%A0%D0%90%D0%A6%D0%98%D0%9E%D0%9D%D0%95.mp4',
                     },
                     {
                         id: '15',
@@ -68,7 +68,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Без белка и жизнь не та. Всё, что нужно знать о белках в рационе',
                         description: 'Урок 4',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%9F%D0%A0%D0%9E%20%D0%92%D0%90%D0%96%D0%9D%D0%9E%D0%A1%D0%A2%D0%AC%20%D0%91%D0%95%D0%9B%D0%9A%D0%90%20%D0%92%20%D0%A0%D0%90%D0%A6%D0%98%D0%9E%D0%9D%D0%95.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%9F%D0%A0%D0%9E%20%D0%92%D0%90%D0%96%D0%9D%D0%9E%D0%A1%D0%A2%D0%AC%20%D0%91%D0%95%D0%9B%D0%9A%D0%90%20%D0%92%20%D0%A0%D0%90%D0%A6%D0%98%D0%9E%D0%9D%D0%95.mp4',
                     },
                     {
                         id: '16',
@@ -76,7 +76,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Женское оздоровление и питание',
                         description: 'Урок 6',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%96%D0%B5%D0%BD%D1%81%D0%BA%D0%BE%D0%B5%20%D0%BE%D0%B7%D0%B4%D0%BE%D1%80%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5.%20%D0%9F%D0%B8%D1%82%D0%B0%D0%BD%D0%B8%D0%B5.%20%D0%96%D0%B8%D1%80%D1%8B%20%D0%B8%20%D0%B1%D0%B5%D0%BB%D0%BA%D0%B8.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%96%D0%B5%D0%BD%D1%81%D0%BA%D0%BE%D0%B5%20%D0%BE%D0%B7%D0%B4%D0%BE%D1%80%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5.%20%D0%9F%D0%B8%D1%82%D0%B0%D0%BD%D0%B8%D0%B5.%20%D0%96%D0%B8%D1%80%D1%8B%20%D0%B8%20%D0%B1%D0%B5%D0%BB%D0%BA%D0%B8.mp4',
                     },
                 ],
             },
@@ -101,7 +101,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Опыт-секрет успеха',
                         description: 'Урок 1',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%9E%D0%9F%D0%AB%D0%A2%20-%20%D0%A1%D0%95%D0%9A%D0%A0%D0%95%D0%A2%20%D0%A3%D0%A1%D0%9F%D0%95%D0%A5%D0%90.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%9E%D0%9F%D0%AB%D0%A2%20-%20%D0%A1%D0%95%D0%9A%D0%A0%D0%95%D0%A2%20%D0%A3%D0%A1%D0%9F%D0%95%D0%A5%D0%90.mp4',
                     },
                     {
                         id: '18',
@@ -109,7 +109,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Выход из матрицы',
                         description: 'Урок 2',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%92%D0%AB%D0%A5%D0%9E%D0%94%20%D0%98%D0%97%20%D0%9C%D0%90%D0%A2%D0%A0%D0%98%D0%A6%D0%AB.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%92%D0%AB%D0%A5%D0%9E%D0%94%20%D0%98%D0%97%20%D0%9C%D0%90%D0%A2%D0%A0%D0%98%D0%A6%D0%AB.mp4',
                     },
                     {
                         id: '19',
@@ -117,7 +117,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Самопрограммирование',
                         description: 'Урок 3',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%A1%D0%B0%D0%BC%D0%BE%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%A1%D0%B0%D0%BC%D0%BE%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5.mp4',
                     },
                     {
                         id: '20',
@@ -125,7 +125,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Манипуляции',
                         description: 'Урок 4',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%9C%D0%B0%D0%BD%D0%B8%D0%BF%D1%83%D0%BB%D1%8F%D1%86%D0%B8%D0%B8.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%9C%D0%B0%D0%BD%D0%B8%D0%BF%D1%83%D0%BB%D1%8F%D1%86%D0%B8%D0%B8.mp4',
                     },
                     {
                         id: '21',
@@ -133,7 +133,7 @@ export const dataCourses: ICourses[] = [
                         title: 'Интуиция',
                         description: 'Урок 5',
                         open: true,
-                        url: 'https://api-wather.plutus-fin.ru/videos/%D0%98%D0%BD%D1%82%D1%83%D0%B8%D1%86%D0%B8%D1%8F.mp4',
+                        url: 'https://content-water.plutus-fin.ru/videos/%D0%98%D0%BD%D1%82%D1%83%D0%B8%D1%86%D0%B8%D1%8F.mp4',
                     },
                 ],
             },
