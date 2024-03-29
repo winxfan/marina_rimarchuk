898224509f4c7f5ac75df1a3ef923f99ea46ddf3
tvdmitry <dmitry.tulupov68@gmail.com>
2024-03-29 14:51:38 +0300

    fix: Fix

--- src/modules/videoBlock/VideoBlock.tsx
+++ src/modules/videoBlock/VideoBlock.tsx
@@ -19,7 +19,7 @@ export const VideoBlock: FC<VideoBlockProps> = () => {
             <Link to="/video" className={css.resetStyle}>
                 <CommonHeader title="Видео от Марины Римарчук" />
             </Link>
-            <CardSlider slidesToShow={2} slidesToShowMobile={1.75}>
+            <CardSlider slidesToShow={2} slidesToShowMobile={1}>
                 {allVideos.data
                     ? allVideos.data?.map((item, index) => <VideoCard key={item.id} {...item} index={index} />)
                     : null}
