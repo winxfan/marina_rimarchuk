44a6d8e38cc4c05bae2b1c0d2c7b84b41dc5a284
tvdmitry <dmitry.tulupov68@gmail.com>
2024-03-31 12:10:39 +0300

    fix: Fix

--- src/modules/media/VideoPlayer.tsx
+++ src/modules/media/VideoPlayer.tsx
@@ -15,10 +15,14 @@ const VideoPlayer: FC<VideoPlayerProps> = (props) => {
 
     return (
         <div className={css.videoPlayerContainer} style={{ maxWidth: width, minHeight: height }}>
-            <video controls width="100%" height="100%">
-                <source src={videoUrl} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <ReactPlayer
+                url={videoUrl}
+                width="100%"
+                height="100%"
+                controls
+                className={css.reactPlayer}
+                light={previewUrl}
+            />
         </div>
     );
 };
