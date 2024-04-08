ae3cd07326f4b427ce0a54a3420025e207d4defc
tvdmitry <dmitry.tulupov68@gmail.com>
2024-04-09 00:05:54 +0300

    fix: Fix

--- src/utils/api/pay.ts
+++ src/utils/api/pay.ts
@@ -8,10 +8,20 @@ export const payContentRequest = async ({
     course_id,
     manuals_id,
 }: Pay): Promise<PayMessage> => {
-    const response = await axios.post(
-        `/api/prodamus/pay?customer_phone=${customer_phone}&customer_email=${customer_email}&cost=${+cost}&course_id=${+course_id}&manuals_id=${+manuals_id}`
-    );
+    try {
+        const response = await axios.post(
+            `/api/prodamus/pay?customer_phone=${customer_phone}&customer_email=${customer_email}&cost=${+cost}&course_id=${+course_id}&manuals_id=${+manuals_id}`
+        );
 
-    console.log(response.data, 'rrrrrrrr');
-    return response.data;
+        console.log(response.data, 'rrrrrrrr');
+        if (response.data.pay_url) {
+            window.location.href = response.data.pay_url;
+
+            return response.data;
+        } else {
+            console.error('Отсутствует URL для редиректа');
+        }
+    } catch (error) {
+        console.error('Ошибка при выполнении запроса:', error);
+    }
 };
