161b2f354bfd880e13dab36dac9263fc5bcaba3c
Danil <92051977+danil-rezbaev@users.noreply.github.com>
2024-04-11 01:39:31 +0200

    code review

--- src/utils/api/videos.ts
+++ src/utils/api/videos.ts
@@ -3,5 +3,6 @@ import { Videos } from '@/utils/types/videos';
 
 export const getAllVideosRequest = async (): Promise<Videos[]> => {
     const response = await axios.get(`/api/videos/getall`);
-    return response.data.data;
+    console.log({videos: response})
+    return response.data.data ?? [];
 };
