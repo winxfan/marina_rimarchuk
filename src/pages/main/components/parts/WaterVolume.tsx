5eb37087e7d103d74b68ce732550ae06e8a5c509
tvdmitry <dmitry.tulupov68@gmail.com>
2024-04-14 10:37:15 +0300

    fix: Fix

--- src/pages/main/components/parts/WaterVolume.tsx
+++ src/pages/main/components/parts/WaterVolume.tsx
@@ -5,7 +5,7 @@ import { ThunkDispatch } from '@reduxjs/toolkit';
 
 import WaterIcon from '@/assets/images/waterTracker/waterIcon.svg';
 import { getWater } from '@/store/waterGetSlice';
-import { AuthResponse, AuthUser } from '@/utils/types';
+import { AuthUser } from '@/utils/types';
 import { GetWaterResponse, WaterData } from '@/utils/types/water';
 
 import css from './WaterVolume.module.scss';
@@ -19,14 +19,14 @@ export const WaterVolume = (props: WaterVolumeProps) => {
     const { sliderValue } = props;
 
     const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
-    const authUser: AuthUser = useSelector((state: AuthResponse) => state.auth);
+    const waterVolume: WaterData = useSelector((state: GetWaterResponse) => state.waterGet);
 
-    /*    useEffect(() => {
+    useEffect(() => {
         console.log(sliderValue, 'sliderValue');
         dispatch(getWater());
-    }, [sliderValue]);*/
+    }, [sliderValue]);
 
-    console.log(authUser, 'authUser 2222');
+    console.log(waterVolume, 'waterVolume111');
 
     return (
         <div className={css.trackerInfo}>
@@ -34,7 +34,7 @@ export const WaterVolume = (props: WaterVolumeProps) => {
                 <WaterIcon />
             </div>
             <div className={css.waterVolume}>
-                <div className={css.volume}>{0} из 2560 мл</div>
+                <div className={css.volume}>{waterVolume.data} из 2560 мл</div>
             </div>
         </div>
     );
