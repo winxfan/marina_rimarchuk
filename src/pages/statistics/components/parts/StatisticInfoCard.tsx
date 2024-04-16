744dfef961a64f250caba58502490b6d5e6b066b
tvdmitry <dmitry.tulupov68@gmail.com>
2024-04-16 22:38:10 +0300

    fix: Fix

--- src/pages/statistics/components/parts/StatisticInfoCard.tsx
+++ src/pages/statistics/components/parts/StatisticInfoCard.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
 
 import cs from 'classnames';
 
 import cameraImage from '@/assets/images/statistics/camera.png';
 import microImage from '@/assets/images/statistics/micro.png';
 import waterImage from '@/assets/images/statistics/water.png';
+import { AuthResponse, AuthUser } from '@/utils/types';
 import { IStatisticInfoCard } from '@/utils/types/statistic';
 
 import css from './StatisticInfoCard.module.scss';
@@ -13,25 +15,37 @@ export type StatisticInfoCardProps = {
     className?: any;
 };
 
-const data: IStatisticInfoCard[] = [
-    { id: '1', title: '0 мл', description: 'за неделю выпито воды', icon: waterImage },
-    { id: '2', title: '0 мл', description: 'за месяц выпито воды', icon: waterImage },
-    { id: '3', title: '0', description: 'Видео вы уже посмотрели', icon: cameraImage },
-    { id: '4', title: '0', description: 'Подкастов вы уже послушали', icon: microImage },
-];
-
 export const StatisticInfoCard: FC<StatisticInfoCardProps> = (props) => {
     const { className } = props;
 
+    const authUser: AuthUser = useSelector((state: AuthResponse) => state.auth);
+    console.log(authUser, 'authUser222');
+
     return (
         <div className={cs(css.statisticInfoCard, className)}>
-            {data.map((item) => (
-                <div key={item.id} className={cs(css.infoCardItem)}>
-                    <img src={item.icon} className={css.infoCardIcon} alt="icon statistic" />
-                    <div className={css.infoCardTitle}>{item.title}</div>
-                    <div className={css.infoCardDescription}>{item.description}</div>
-                </div>
-            ))}
+            <div className={cs(css.infoCardItem)}>
+                <img src={waterImage} className={css.infoCardIcon} alt="icon statistic" />
+                <div className={css.infoCardTitle}>1 мл</div>
+                <div className={css.infoCardDescription}>за неделю выпито воды</div>
+            </div>
+
+            <div className={cs(css.infoCardItem)}>
+                <img src={waterImage} className={css.infoCardIcon} alt="icon statistic" />
+                <div className={css.infoCardTitle}>1 мл</div>
+                <div className={css.infoCardDescription}>за месяц выпито воды</div>
+            </div>
+
+            <div className={cs(css.infoCardItem)}>
+                <img src={cameraImage} className={css.infoCardIcon} alt="icon statistic" />
+                <div className={css.infoCardTitle}>1</div>
+                <div className={css.infoCardDescription}>Видео вы уже посмотрели</div>
+            </div>
+
+            <div className={cs(css.infoCardItem)}>
+                <img src={microImage} className={css.infoCardIcon} alt="icon statistic" />
+                <div className={css.infoCardTitle}>1</div>
+                <div className={css.infoCardDescription}>Подкастов вы уже послушали</div>
+            </div>
         </div>
     );
 };
