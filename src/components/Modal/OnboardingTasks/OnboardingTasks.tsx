5ce0e4240cb96bd37c064ee4fa96a4b89994856f
tvdmitry <dmitry.tulupov68@gmail.com>
2024-06-29 14:21:55 +0300

    fix: fix

--- src/components/Modal/OnboardingTasks/OnboardingTasks.tsx
+++ src/components/Modal/OnboardingTasks/OnboardingTasks.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
